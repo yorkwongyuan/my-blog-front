42490d5b957df5f82568de33ca872f72e7fc5c71
yuan_wy_wang <631824375@qq.com>
2022-08-23 21:09:13 +0800

    feat: test

--- src/components/gov-about/beian.vue
+++ src/components/gov-about/beian.vue
@@ -18,7 +18,7 @@
 </style>
 <template>
   <div class="beian">
-    <img class="beian__img" src="./imgs/备案图标.png" alt="" />
+    <img class="beian__img" src="./imgs/备案图标.png" alt="图案" />
     <div style="width:300px;margin:0 auto; padding:20px 0;">
       <a class="beian__link" target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=44011302003676" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
         <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px;">
