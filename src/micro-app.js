7165491fae5fa17ecdbe521b5531735f8c49a83d
yuan_wy_wang <631824375@qq.com>
2022-09-06 22:10:01 +0800

    fix: https

--- src/micro-app.js
+++ src/micro-app.js
@@ -1,7 +1,7 @@
 const microApps = [
   {
     name: 'sub-vue',
-    entry: process.env.NODE_ENV === 'production' ? 'http://119.91.228.12:9661' : '//localhost:7788/',
+    entry: process.env.NODE_ENV === 'production' ? 'http://blog.wyuan.vip' : '//localhost:7788/',
     container: '#container', // 子应用挂在节点
     activeRule: '/sub-vue',
     props: {
